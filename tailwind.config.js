3d6cd1aab93639f74fb7b9fb61a0c36ccdb6af14
Noémie ROCHARD <nrochard@student.42.fr>
2022-06-09 14:43:38 +0200

    header + hero

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["./src/**/*.{html,js}", "./public/*.html", "./src/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
